68f4ce1f13f686c66f317e6fdd51bd9d38857770
“Lissa <lwarshaw674@gmail.com>
2023-04-11 15:37:22 -0400

    removing portions of team schema

--- models/Team.js
+++ models/Team.js
@@ -3,8 +3,8 @@ const Schema = mongoose.Schema;
 
 const Team = new Schema({
   teamName: { type: String },
-  teamID: { type: Number },
-  previousNames: [{ type: String }],
+  // teamID: { type: Number },
+  // previousNames: [{ type: String }],
   principal: { type: String },
   owner: { type: String },
   engine: { type: String },
