5e075fd19abc2666c55b3e0fa4dc4a6630ee1cc8
“Lissa <lwarshaw674@gmail.com>
2023-04-11 16:02:44 -0400

    mapping

--- seed/data.js
+++ seed/data.js
@@ -4,14 +4,38 @@ import Driver from "../models/Driver.js";
 import teams from "./teams.json" assert { type: "json" };
 import drivers from "./drivers.json" assert { type: "json" };
 
+let driverData = drivers.map(item => {
+  return {
+  name: item.name,
+  team: item.team,
+  podiums: item.podiums,
+  credit: item.copyright,
+  pointsByYear: item.pointsByYear[0],
+  image: item.image
+  }
+});
+
+let teamData = teams.map(item => {
+  return {
+    teamName: item.teamName,
+    owner: item.owner,
+    principal: item.principal,
+    engine: item.engine,
+    championships: item.championships,
+    pointsByYear: item.pointsByYear[0],
+    seasons: item.seasons[0]
+  }
+});
+
+
 const insertData = async () => {
   try {
     // Reset Database
     await db.dropDatabase();
 
     // Insert Drivers & Teams into the Database
-    await Team.insertMany(teams);
-    await Driver.insertMany(drivers);
+    await Team.insertMany(teamData);
+    await Driver.insertMany(driverData);
 
     // Close DB connection
     await db.close();
