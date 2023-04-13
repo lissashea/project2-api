17c3c36f44aebcf89b3a05648dc3e455ba5555e2
“Lissa <lwarshaw674@gmail.com>
2023-04-13 13:56:43 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,16 @@ import teamsRoutes from "./teams.js";
 
 const router = Router();
 
-router.get("/", (req, res) => res.send("This is the api root!"));
+// router.get("/", (req, res) => res.send("This is the api root!"));
+
+router.get("/", async (req, res, next) => {
+  try {
+    const drivers = await Driver.find();
+    res.json(drivers);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.use("/drivers", driversRoutes);
 router.use("/teams", teamsRoutes);
